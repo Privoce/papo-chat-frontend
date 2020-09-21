b62121f4a0433d5236d336ae3286d97795270a7a
Victoria <vivitrindade97@gmail.com>
2020-09-21 17:22:31 -0300

    search fixed

--- src/shared/components/icon/index.jsx
+++ src/shared/components/icon/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { BsSearch } from 'react-icons/bs';
 function IconComponent({ fill, icon, width, height, margin }) {
   function renderCheckedIcon() {
     return (
@@ -113,24 +113,7 @@ function IconComponent({ fill, icon, width, height, margin }) {
   }
 
   function renderSearchIcon() {
-    return (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        version="1.1"
-        width={width}
-        height={height}
-        viewBox="0 0 24 24"
-        className="fadeIn"
-        style={{
-          margin,
-        }}
-      >
-        <path
-          d="M9.5,3A6.5,6.5 0 0,1 16,9.5C16,11.11 15.41,12.59 14.44,13.73L14.71,14H15.5L20.5,19L19,20.5L14,15.5V14.71L13.73,14.44C12.59,15.41 11.11,16 9.5,16A6.5,6.5 0 0,1 3,9.5A6.5,6.5 0 0,1 9.5,3M9.5,5C7,5 5,7 5,9.5C5,12 7,14 9.5,14C12,14 14,12 14,9.5C14,7 12,5 9.5,5Z"
-          fill={fill}
-        />
-      </svg>
-    );
+    return <BsSearch color="#4064D1" />;
   }
 
   function renderArrowLeftIcon() {
