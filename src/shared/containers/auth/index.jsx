68eb10581fb6cbe30597133971447ad1054f8b90
Tulio Calil <tuliocll@gmail.com>
2020-09-30 09:02:15 -0300

    Revert "starting wpp UI"

--- src/shared/containers/auth/index.jsx
+++ src/shared/containers/auth/index.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import logo from '../../../assets/images/logo.png';
+
 function AuthContainer({ title, formContainer, footerInfo, footerInfoLink }) {
   return (
     <section className="auth-wrapper">
       <div className="form-container">
+        <img src={logo} alt="Alora Main Logo" className="logo-image" />
         <h1 className="title">{title}</h1>
         {formContainer}
         <div className="info-container">
