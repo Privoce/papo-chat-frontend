5a5193ff31cd2846db4633edce0c24aa25c56207
Thiago Moreira <gamazyn@live.com>
2021-02-04 17:14:19 -0300

    🔊 Incoming Call tone implemented

--- src/shared/components/incoming-call-window/index.jsx
+++ src/shared/components/incoming-call-window/index.jsx
@@ -7,6 +7,7 @@ import PropTypes from 'prop-types';
 import { MdCall, MdCameraAlt, MdCallEnd } from 'react-icons/md';
 
 import placeholderImg from '../../../assets/images/no-picture.png';
+import IncomingCall from '../../../assets/audio/incoming-call.mp3';
 
 function StartCallWindow({
   nickname,
@@ -19,6 +20,10 @@ function StartCallWindow({
       <img src={image || placeholderImg} alt="User profile" />
       <h2>{nickname || 'Usuario'}</h2>
       <p>Is calling...</p>
+      <audio controls style={{ display: 'none' }} autoPlay loop>
+        <source src={IncomingCall} type="audio/mp3" />
+        Your browser does not support the audio element.
+      </audio>
       <div className="button-container">
         <button type="button" onClick={() => handleAceptCall('audio')}>
           <MdCall />
